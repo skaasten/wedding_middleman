0d95a7a52fdc9bb941fa3bd6fca3d5b1b08f2bbe
Shaun Kaasten <shaunk@gmail.com>
2015-02-28 15:42:58 -0500

    implement rsvp sending

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,21 @@
+//= require vendor/jquery-1.8.0.min.js
 
+$(function() {
+  $("#rsvp-form").submit(function(e) {
+    e.preventDefault();
+    var params = {
+        name: $("input[name=name]").val(),
+        guest: $("input[name=guest]").val(),
+        meal: $("input[name=meal]").val()
+    };
+    $.ajax({
+        url: "http://localhost:3000/rsvp",
+        dataType: "post",
+        type: "POST"
+    }).done(function() {
+        alert("Thank you for rsvping! Can't wait to see you there!");
+    }).error(function() {
+        console.log(arguments);
+    });
+  });
+});
