d531c501f9e15d12d9bd76b91857d4d2ae5b9077
Shaun Kaasten <shaunk@gmail.com>
2015-04-19 21:46:51 -0400

    Pass in var for can_attend

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,12 @@
 //= require vendor/jquery-1.8.0.min.js
 
 $(function() {
+
+  $("input[name=can_attend]").change(function() {
+    $(".meal-choice").toggleClass("hidden", $(this).val());
+    $(".guest-name").toggleClass("hidden", $(this).val());
+  });
+
   $("#rsvp-form").submit(function(e) {
     e.preventDefault();
     var params = {
@@ -8,8 +14,10 @@ $(function() {
         name: $("input[name=name]").val(),
         meal_choice: $("select[name=meal_choice]").val(),
         guest: $("input[name=guest]").val(),
-        guest_meal: $("select[name=guest_meal]").val()
+        guest_meal: $("select[name=guest_meal]").val(),
+        can_attend: $('input[name="can_attend"]:checked').val()
     };
+
     $.ajax({
         url: "/rsvp/rsvp",
         contentType : 'application/json',
